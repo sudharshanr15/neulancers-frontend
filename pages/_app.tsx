b754efdc4684612fc031ccac9bb3228b9cf87b91
Sudharshan <sudharshanrms2004@gmail.com>
2023-02-12 00:18:38 +0530

    Hero banner section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import '@/styles/globals.css'
 import '@/styles/app.css'
 import type { AppProps } from 'next/app'
 import Navbar from "../components/Navbar"
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" integrity="sha512-YFENbnqHbCRmJt5d+9lHimyEMt8LKSNTMLSaHjvsclnZGICeY/0KYEeiHwD1Ux4Tcao0h60tdcMv+0GljvWyHg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </Head>
       <Navbar/>
       <Component {...pageProps} />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link"
+
 export default function Home(){
   return (
-    <div className="container-2">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <section className="hero-section">
+        <div className="hero-section-container">
+          <div className="row">
+            <div className="col-6">
+              <div className="hero-title-container">
+                <h1 className="hero-title">Best Software developers in Chennai.</h1>
+                <p className="hero-title-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim harum sapiente totam consequuntur accusamus perspiciatis saepe laborum ea eveniet atque?</p>
+              </div>
+              <Link href="" className="btn get-started-btn">
+                <span className="btn-text">Get Started</span>
+                <span className="arrow-span"><i className="bi bi-arrow-right-short fs-3"></i></span>
+              </Link>
+            </div>
+            <div className="col-6">
+              <p>Hello</p>
+            </div>
+          </div>
+          <div className="intro-container">
+            <h3 className="intro-title">WE ARE NEULANCERS</h3>
+            <p className="intro-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quam quidem perferendis molestias a ut ullam recusandae fuga delectus commodi?</p>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
