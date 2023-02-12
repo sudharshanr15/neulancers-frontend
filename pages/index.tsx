e6d894ddc4a7212b37e880f08d65c847316e98ee
Sudharshan <sudharshanrms2004@gmail.com>
2023-02-12 16:06:53 +0530

    About us section

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,21 @@ export default function Home(){
           </div>
         </div>
       </section>
+      <section className="about-us-section">
+        <div className="about-us-section-container">
+          <div className="row">
+            <div className="col-6">
+
+            </div>
+            <div className="col-6 about-us-content-container">
+              <h2 className="about-us-title">About Us</h2>
+              <h3 className="about-us-title-text">Innovating with a Digitial Mindset</h3>
+              <p className="about-us-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. In ab, eligendi fugiat architecto officia obcaecati qui dicta dolorem. Quo cum recusandae perspiciatis debitis modi ut officia reiciendis dolor repellat numquam?</p>
+              <Link href="" className="contact-us-link">Contact Us</Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
