8fe549e328a643ca3e9f26726cecd74317e600a6
Sudharshan <sudharshanrms2004@gmail.com>
2023-05-17 22:56:19 +0530

    Added href for navbar items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar(){
       <>
         <nav className="navbar navbar-expand-lg">
           <div className="container-fluid navbar-content">
-            <Link className="navbar-brand" href="#">
+            <Link className="navbar-brand" href="/">
                 <Image className="navbar-img" alt="Neulancers logo" src={'/assets/images/neulancers-logo.svg'} height="60" width="60"/>
             </Link>
             <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,13 +16,16 @@ function Navbar(){
               <div className="nav-bar-grp">
                 <ul className="navbar-nav">
                   <li className="nav-item">
-                    <Link className="nav-link active" href="#">Home</Link>
+                    <Link className="nav-link active" href="/">Home</Link>
                   </li>
                   <li className="navbar-item">
-                    <Link className="nav-link active" href="#">Regarding Us</Link>
+                    <Link className="nav-link active" href="/about">Regarding Us</Link>
                   </li>
                   <li className="navbar-item">
-                    <Link className="nav-link active" href="#">Our Works</Link>
+                    <Link className="nav-link active" href="/wedo">What we do</Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link className="nav-link active" href="/rd">R&D</Link>
                   </li>
                 </ul>
                 <Link href={'/'}>
